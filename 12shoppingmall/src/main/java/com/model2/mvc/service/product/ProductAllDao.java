e266488cc51f4fb875119d60297799534b86e6ce
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2024-05-06 10:57:21 +0900

    24/05/06 :: 0.7.1
    다시 dbcp를 넣고 돌렸는데, 이번에는 @Mapper bean을 @Autowired로 주입하는 과정에서 오류가 발생했다.
    
     [Catalina-utility-2] WARN
    org.springframework.web.context.support.XmlWebApplicationContext -
    Exception encountered during context initialization - cancelling refresh
    attempt: org.springframework.beans.factory.UnsatisfiedDependencyException:
    Error creating bean with name 'productAllDao': Unsatisfied dependency
    expressed through field 'productDao'; nested exception is
    org.springframework.beans.factory.NoSuchBeanDefinitionException: No
    qualifying bean of type 'com.model2.mvc.service.product.ProductDao'
    available: expected at least 1 bean which qualifies as autowire
    candidate. Dependency annotations:
    {@org.springframework.beans.factory.annotation.Autowired(required=true)}
    
     [Catalina-utility-2] ERROR
    org.springframework.web.context.ContextLoader - Context initialization
    failed
    
    productAllDao는 여러 Dao 객체들을 모아주는 adapter 같은 역할을 한다. 이 녀석에게 @Component를
    부여했는데, 이것이 문제일 것 같다는 생각은 안 들지만 일단 @Service로 변경해보았다. 가장 의심되는 츠측 point는
    @Mapper 로 생성된 Proxy 객체를 주입하지 못하는 것 같다는 점이다.

--- 12shoppingmall/src/main/java/com/model2/mvc/service/product/ProductAllDao.java
+++ 12shoppingmall/src/main/java/com/model2/mvc/service/product/ProductAllDao.java
@@ -3,11 +3,13 @@ package com.model2.mvc.service.product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import lombok.Getter;
 
 @Getter
-@Component
+// @Component
+@Service
 public class ProductAllDao {
 	
 	@Autowired
