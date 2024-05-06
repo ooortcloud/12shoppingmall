1a697391c48261471879266eb88a3d81b5806ec3
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2024-05-07 08:27:49 +0900

    24/05/07 : 0.7.10
    MapperScan을 걸어주는 것이 맞았다. @MapperScan을 filtering과 함께 적용하여 문제를 해결해보자.
    (Application.java에 @MapperScan을 정의하였다.)

--- 12shoppingmall/src/main/java/com/model2/mvc/Application.java
+++ 12shoppingmall/src/main/java/com/model2/mvc/Application.java
@@ -1,9 +1,13 @@
 package com.model2.mvc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 구성 class를 따로 만들어주면 좋을 거 같은데...
+@MapperScan(basePackages="com.model2.mvc.service",
+	annotationClass = org.apache.ibatis.annotations.Mapper.class )
 public class Application {
 
 	public static void main(String[] args) {
